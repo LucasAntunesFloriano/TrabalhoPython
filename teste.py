6dfc1a7c681ae02a55c7dbf02dbc29ca95c51032
LucasAntunesFloriano <50336964+LucasAntunesFloriano@users.noreply.github.com>
2025-11-14 20:15:40 -0300

    Trab sexta

--- teste.py
+++ teste.py
@@ -4,7 +4,6 @@ import PyPDF2
 import requests
 import re
 
-
 def ChamadaAPI(Ano):
     url = "https://date.nager.at/api/v3/PublicHolidays/"+Ano+"/BR"
 
@@ -16,33 +15,36 @@ def ChamadaAPI(Ano):
     response = requests.request("GET", url, headers=headers, data=payload)
 
     return response.text
-
+    
 def Get_text_from_PDFfiles_usingPyPDF2(in_PdfFile):  
     reader = PyPDF2.PdfReader(in_PdfFile) 
     return reader.pages[0].extract_text()
 
-def lerPdf():
+    
+def LerPdf():
     filename = askopenfilename()
-    ConteudoPDF = Get_text_from_PDFfiles_usingPyPDF2(filename)
+    TextoPDF = Get_text_from_PDFfiles_usingPyPDF2(filename)
+    ListaAnos = set(re.findall("\d{4}(?=-\d{2}-\d{2})", TextoPDF))
 
-    x = re.findall("\d{4}(?=-\d{2}-\d{2})", ConteudoPDF)
-    ConteudoAPI=""
-    for Ano in set(x):
-        ConteudoAPI = ConteudoAPI + ChamadaAPI(Ano)
-    for DataPdf in ConteudoPDF.split("\n"):
-        if DataPdf.strip() in ConteudoAPI:
-            print(DataPdf)
+    RespAPI = ""
+    for Ano in ListaAnos:
+        RespAPI = RespAPI + ChamadaAPI(Ano)
+        
+    for Data in TextoPDF.split("\n"):
+        if Data.strip() in RespAPI:
+            print(Data)
 
 def Exemplo():
     root = tkinter.Tk()
     root.title("Titulo da janela")
     root.resizable(True, True)
     
-    test = tkinter.Button(root, text="Escolher PDF")
-    test['command'] = lerPdf
+    test = tkinter.Button(root, text="Ler pdf")
+    test['command'] = LerPdf
     test.pack()
 
-    
     root.mainloop()
-
+    
 Exemplo()
+
+
